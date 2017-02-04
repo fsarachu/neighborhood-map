6e8171587bf1ae02b0e9d6f7ee0fea4a10b09ccd
fsarachu <fsarachu@gmail.com>
2017-02-04 20:21:24 -0300

    Fix menu position after window resize, using debouncing

--- js/sideMenu.js
+++ js/sideMenu.js
@@ -1,15 +1,26 @@
 $(function () {
+    // Cache some elements
     var $sideMenu = $(".side-menu");
     var $menuToggle = $sideMenu.find(".menu-toggle");
     var $menuContent = $sideMenu.find(".menu-content");
     var $menuToggleIcon = $menuToggle.find(".fa");
 
+    // First, close the menu
     menuToggle();
 
+    // Toggle menu when toggle area is clicked
     $menuToggle.on('click', function () {
         menuToggle();
     });
 
+    // If window is resized menu can get resized, so if it's closed we should check if it's still in place.
+    $(window).on("resize", debounce(function () {
+        if ($sideMenu.css("left") != "0px") {
+            $sideMenu.css("left", -$menuContent.outerWidth() + "px");
+        }
+    }, 250));
+
+    // Toggle opened/closed menu state
     function menuToggle() {
         console.log($sideMenu.css("left"));
 
@@ -22,3 +33,21 @@ $(function () {
         }
     }
 });
+
+// Returns a function, that, as long as it continues to be invoked, will not be triggered.
+// The function will be called after it stops being called for N milliseconds.
+// If `immediate` is passed, trigger the function on the leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
