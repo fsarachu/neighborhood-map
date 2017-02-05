68c11076ffabaccb9d1f31f8ccfb4cab3ea747e4
fsarachu <fsarachu@gmail.com>
2017-02-05 08:24:33 -0300

    Center map on approximate user's current location

--- app/js/map.js
+++ app/js/map.js
@@ -5,4 +5,8 @@ function initMap() {
         center: {lat: 40.705311, lng: -74.2581952},
         zoom: 8
     });
+
+    getUserLocation(function (lat, lng) {
+        map.setCenter({"lat": parseInt(lat), "lng": parseInt(lng)});
+    });
 }
