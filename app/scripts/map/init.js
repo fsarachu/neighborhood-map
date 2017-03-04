ca5fab16abb7bffc5959238dca42b73e540be32b
fsarachu <fsarachu@gmail.com>
2017-03-04 15:38:09 -0300

    Enable user geolocation on map init again

--- app/scripts/map/init.js
+++ app/scripts/map/init.js
@@ -1,21 +1,28 @@
 import loadGoogleMapsApi from 'load-google-maps-api';
-// import geolocate from './geolocator';
+import geolocate from './geolocator';
+
+let map = null, userMarker = null;
 
 export default function initMap(callback) {
   loadGoogleMapsApi({
     key: 'AIzaSyClOMwnqYq0BzWIu4XvFHY_FJ20w3PZ5cw',
     libraries: ['places']
   }).then((googleMaps) => {
-    let map = new googleMaps.Map(document.getElementById('map'), {
+    map = new googleMaps.Map(document.getElementById('map'), {
       zoom: 3,
       center: {lat: 0, lng: 0},
       gestureHandling: 'cooperative'
     });
 
-    // geolocate((position) => {
-    //   map.setCenter(position);
-    //   map.setZoom(8);
-    // });
+    geolocate((position) => {
+      map.setCenter(position);
+      map.setZoom(14);
+
+      userMarker = new googleMaps.Marker({
+        position: position,
+        map: map
+      });
+    });
 
     callback(map);
   }).catch((err) => {
