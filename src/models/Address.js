e55a2557b4c10c58d2c2c27c6e41eb9c3cc5d1a6
fsarachu <fsarachu@gmail.com>
2017-05-01 23:22:36 -0300

    Reformat file

--- src/models/Address.js
+++ src/models/Address.js
@@ -1,9 +1,11 @@
 import ko from 'knockout';
 
 export default class Address {
+
   constructor() {
     this.name = ko.observable();
     this.marker = ko.observable();
     this.location = ko.observable();
   }
+
 }
