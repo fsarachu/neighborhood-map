02be1476678369b5bea86dcda7dce3100d82105b
fsarachu <fsarachu@gmail.com>
2017-05-01 23:22:11 -0300

    Create Address model

---
+++ src/models/Address.js
@@ -0,0 +1,9 @@
+import ko from 'knockout';
+
+export default class Address {
+  constructor() {
+    this.name = ko.observable();
+    this.marker = ko.observable();
+    this.location = ko.observable();
+  }
+}
