16435994da2bd545d1228033b95c921ab26861a4
fsarachu <fsarachu@gmail.com>
2017-05-02 21:13:57 -0300

    Use bootstrap in main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,1 @@
-import 'material-design-lite/dist/material';
-import ko from 'knockout';
-
-window.initMap = function () {
-  let google = window.google;
-  window.map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 0, lng: 0},
-    zoom: 2
-  });
-}
+import './bootstrap';
