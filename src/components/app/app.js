add7858717e69ceed4dad78aceed199893d48cec
fsarachu <fsarachu@gmail.com>
2017-05-06 19:23:43 -0300

    Remove test code

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,31 +11,6 @@ class App {
     this.services = {
       location: LocationService,
     };
-
-    this.services.location.loadData([
-      {
-        name: 'First Location',
-        position: {
-          lat: 12.345,
-          lng: 8.123
-        }
-      },
-      {
-        name: 'Second Location',
-        position: {
-          lat: 4.345,
-          lng: -5.123
-        }
-      },
-      {
-        position: {
-          lat: 'a',
-          lng: -5.123
-        }
-      }
-    ]);
-
-    console.dir(this.services.location.locations().map(location => location.toStorable()));
   }
 }
 
