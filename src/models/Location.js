fc0b59aca42461dec28aeb7319b9000b12fd17ba
fsarachu <fsarachu@gmail.com>
2017-05-06 18:26:04 -0300

    Update "Location" model. Extend "Model" base class and override toStorable()

--- src/models/Location.js
+++ src/models/Location.js
@@ -1,14 +1,25 @@
 import ko from 'knockout';
+import Model from "../core/Model";
 
-export default class Location {
-
+export default class Location extends Model {
   constructor(data) {
+    super();
     this.name = ko.observable(data.name);
     this.position = {
       lat: ko.observable(data.position.lat),
       lng: ko.observable(data.position.lng),
     };
     this.marker = ko.observable();
+  }
+
+  toStorable() {
+    return {
+      name: this.name(),
+      position: {
+        lat: this.position.lat,
+        lng: this.position.lng
+      },
+    };
   }
 
 }
