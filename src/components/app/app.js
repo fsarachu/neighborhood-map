062eac0cb69b63138cfe6740beed4284d5a0692e
fsarachu <fsarachu@gmail.com>
2017-05-06 19:15:36 -0300

    Test LocationService

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,6 +8,34 @@ class App {
       lng: ko.observable(0),
     };
     this.zoom = ko.observable(2);
+    this.services = {
+      location: LocationService,
+    };
+
+    this.services.location.loadData([
+      {
+        name: 'First Location',
+        position: {
+          lat: 12.345,
+          lng: 8.123
+        }
+      },
+      {
+        name: 'Second Location',
+        position: {
+          lat: 4.345,
+          lng: -5.123
+        }
+      },
+      {
+        position: {
+          lat: 'a',
+          lng: -5.123
+        }
+      }
+    ]);
+
+    console.dir(this.services.location.locations().map(location => location.toStorable()));
   }
 }
 
