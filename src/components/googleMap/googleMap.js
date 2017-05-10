d4f4c0df88fe583f791c50ae5cb40c62f1aced64
fsarachu <fsarachu@gmail.com>
2017-05-10 02:30:35 -0300

    Refactor googleMap.js. Remove geolocate() method and unneeded options (They are already in the viewModel instance).

--- src/components/googleMap/googleMap.js
+++ src/components/googleMap/googleMap.js
@@ -4,19 +4,11 @@ import geolocator from 'geolocator';
 
 ko.bindingHandlers.map = {
   init(element, value, all, vm) {
-    let options = {
-      center: {
-        lat: ko.unwrap(all.get('lat')),
-        lng: ko.unwrap(all.get('lng')),
-      },
-      zoom: ko.unwrap(all.get('zoom')),
-      geolocate: ko.unwrap(all.get('geolocate')),
-    };
-
-    vm.loadMap(options);
+    vm.loadMap();
   },
   update (element, value, all, vm) {
     let map = ko.unwrap(all.get('map'));
+
     if (map) {
       map.setZoom(ko.unwrap(all.get('zoom')));
       map.setCenter({
@@ -24,6 +16,7 @@ ko.bindingHandlers.map = {
         lng: ko.unwrap(all.get('lng')),
       });
     }
+
   }
 }
 
@@ -36,51 +29,24 @@ class Map {
     this.isLoading = ko.computed(() => !this.map());
   }
 
-  loadMap(options) {
+  loadMap() {
     loadGoogleMapsAPI({key: 'AIzaSyClOMwnqYq0BzWIu4XvFHY_FJ20w3PZ5cw'})
       .then(gmaps => {
         this.googleMaps = gmaps;
-        this.createMap(options.geolocate);
+        this.createMap();
       })
       .catch(e => console.error(e));
   }
 
-  createMap(geolocate) {
+  createMap() {
     let map = new this.googleMaps.Map(document.getElementById('map'), {
       center: new this.googleMaps.LatLng(this.center().lat(), this.center().lng()),
       zoom: this.zoom(),
     });
 
     this.map(map);
-
-    if (geolocate) {
-      this.geolocate();
-    }
   }
 
-  geolocate() {
-    let options = {
-      addressLookup: false,
-      desiredAccuracy: 30,
-      enableHighAccuracy: true,
-      fallbackToIP: true,
-      maximumAge: 0,
-      maximumWait: 10000,
-      staticMap: false,
-      timeout: 5000,
-      timezone: false,
-    };
-
-    geolocator.locate(options, (err, location) => {
-      if (err) {
-        console.log(err);
-      }
-
-      this.center().lat(location.coords.latitude);
-      this.center().lng(location.coords.longitude);
-      this.zoom(14);
-    });
-  }
 }
 
 export default {
