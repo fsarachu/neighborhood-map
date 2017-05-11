03ba1c35b803f9f22aa70fae72d691632a4a6b7d
fsarachu <fsarachu@gmail.com>
2017-05-10 23:39:48 -0300

    Update LocationService.validateLocationData(). Add required properties and improve error messages

--- src/services/LocationService.js
+++ src/services/LocationService.js
@@ -46,20 +46,22 @@ class LocationService extends DataService {
     let hasErrors = false;
 
     let requiredProperties = [
-      'name',
-      'position',
       'highlighted',
+      'id',
+      'name',
+      'neighborhoodId',
+      'position',
     ];
 
     for (let property of requiredProperties) {
       if (!(property in locationData)) {
-        this.errors.push(new DataError(`Missing required property: "${property}"`, locationData));
+        this.errors.push(new DataError(`Missing required Location property: "${property}"`, locationData));
         hasErrors = true;
       }
     }
 
     if (locationData.position && (!('lat' in locationData.position) || !('lng' in locationData.position) || typeof locationData.position.lat != 'number' || typeof locationData.position.lng != 'number')) {
-      this.errors.push(new DataError('Invalid position property', locationData));
+      this.errors.push(new DataError('Invalid position Location property', locationData));
       hasErrors = true;
     }
 
