69741bae8eb980400b3861a414e0407d34b6f4d3
fsarachu <fsarachu@gmail.com>
2017-06-01 19:45:00 -0300

    Setup simple router test

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,33 +1,4 @@
-import ko from "knockout";
-import postal from "postal";
-import NeighborhoodService from "../../services/NeighborhoodService";
-
 class App {
-  constructor() {
-    this.channels = {
-      app: postal.channel('app'),
-      googleMap: postal.channel('googleMap'),
-    };
-    this.neighborhood = ko.observable(NeighborhoodService.get());
-    this.menuTitle = ko.computed(() => this.neighborhood() ? this.neighborhood().name() : 'My Neighborhood');
-    this.showMenu = ko.computed(() => !!this.neighborhood());
-    this.init();
-  }
-
-  init() {
-    this.postalSubscribe();
-    if (!this.neighborhood()) {
-      console.log('no neighborhood..');
-      setTimeout(() => this.channels.app.publish('welcomeDialog', {show: true}), 0); // Send to event queue
-    }
-  }
-
-  postalSubscribe() {
-    this.channels.googleMap.subscribe("mapClick", function (data, envelope) {
-      console.log('From app:', data);
-    });
-  }
-
 }
 
 export default {
