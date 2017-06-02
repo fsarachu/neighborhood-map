232bb63a0fb9b71d350f0c60a1970860d59741e5
fsarachu <fsarachu@gmail.com>
2017-06-01 22:31:41 -0300

    Remove welcomeDialog component, will use sweet alert

--- src/bootstrap/modules/components.js
+++ src/bootstrap/modules/components.js
@@ -3,9 +3,7 @@ import ko from "knockout";
 import AppComponent from "../../app/components/app/app";
 import GoogleMapComponent from "../../app/components/googleMap/googleMap";
 import NeighborhoodMapComponent from "../../app/components/neighborhoodMap/neighborhoodMap";
-import WelcomeDialogComponent from "../../app/components/welcomeDialog/welcomeDialog";
 
 ko.components.register('app', AppComponent);
 ko.components.register('app-google-map', GoogleMapComponent);
 ko.components.register('app-neighborhood-map', NeighborhoodMapComponent);
-ko.components.register('app-welcome-dialog', WelcomeDialogComponent);
