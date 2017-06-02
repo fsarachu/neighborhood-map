4ea9a74b7a813d55723e3fc38a5d8dd0bf51aef6
fsarachu <fsarachu@gmail.com>
2017-06-02 00:39:24 -0300

    Sample welcome sweet alert

--- src/app/views/welcome/welcome.js
+++ src/app/views/welcome/welcome.js
@@ -1,6 +1,15 @@
+import swal from "sweetalert2";
+
 class Welcome {
 
   constructor(ctx) {
+    swal({
+      title: 'Welcome!',
+      text: 'It looks like it\'s your first time here... Let\'s create your neighborhood!',
+      confirmButtonText: 'Get Started',
+      confirmButtonClass: 'mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored',
+      buttonsStyling: false,
+    });
   }
 
 }
