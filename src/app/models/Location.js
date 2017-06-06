cb0346d2d07b768293e43eef57f5a8ae2d2c5b79
fsarachu <fsarachu@gmail.com>
2017-06-05 22:49:11 -0300

    Remove toStorable() from "Location" model

--- src/app/models/Location.js
+++ src/app/models/Location.js
@@ -3,6 +3,7 @@ import Model from "../../base/Model";
 import NeighborhoodService from '../services/NeighborhoodService';
 
 export default class Location extends Model {
+
   constructor(data) {
     super(data.id);
     this.name = ko.observable(data.name);
@@ -14,18 +15,6 @@ export default class Location extends Model {
     this.neighborhoodId = ko.observable(data.neighborhoodId);
     this.neighborhood = ko.computed(() => NeighborhoodService.get(this.neighborhoodId()));
     this.marker = ko.observable();
-  }
-
-  toStorable() {
-    return {
-      name: this.name(),
-      position: {
-        lat: this.position.lat(),
-        lng: this.position.lng(),
-      },
-      highlighted: this.highlighted(),
-      neighborhoodId: this.neighborhoodId(),
-    };
   }
 
 }
