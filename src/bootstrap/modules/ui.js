57b83486cdf0df8c500ba8f2d04dfbae53c0b8d4
fsarachu <fsarachu@gmail.com>
2017-07-02 22:50:38 -0300

    Remove test notification

--- src/bootstrap/modules/ui.js
+++ src/bootstrap/modules/ui.js
@@ -4,6 +4,3 @@ import "uikit/dist/css/uikit.css";
 
 // loads the Icon plugin
 UIkit.use(Icons);
-
-// components can be called from the imported UIkit reference
-UIkit.notification('Hello world.');
